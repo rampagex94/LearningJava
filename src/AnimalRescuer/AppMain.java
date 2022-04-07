dcb477098ed9242dbbbc7842c34c079a5c1c516f
Silviu Filip <61214836+rampagex94@users.noreply.github.com>
2022-04-07 03:40:05 +0200

    a

--- src/AnimalRescuer/AppMain.java
+++ src/AnimalRescuer/AppMain.java
@@ -1,4 +1,24 @@
 package AnimalRescuer;
 
 public class AppMain {
+
+    public static void main(String[] args) {
+       Dog constantin = new Dog();
+       int x = 3;
+       System.out.println("Varsta catelului este de " + constantin.ageInMonths + " de luni");
+
+       constantin.name = "Trex";
+       constantin.race = "";
+        System.out.println(constantin.name);
+        constantin.run();
+
+        Dog const2 = new Dog();
+        System.out.println(constantin.name);
+        System.out.println(const2.name);
+
+    }
+
+
+
 }
+
---
+++ src/AnimalRescuer/Dog.java
@@ -0,0 +1,28 @@
+package AnimalRescuer;
+
+public class Dog {
+
+    String color = "black";
+    double weight = 23.5;
+    int ageInMonths = 24;
+    String ownerName = "Lisa";
+    String race = "doberman";
+    String name = "Athena";
+    boolean isFemale = true;
+
+    public void eat (){
+        System.out.println("Eating dog food");
+    }
+
+    public void sleep(){
+        System.out.println("zZzZzzZz");
+    }
+
+    public void run(){
+        System.out.println("Running");
+    }
+
+    public void play(){
+        System.out.println("Playing");
+    }
+}
