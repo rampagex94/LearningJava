942deba9e3a274ad676a0a6d9367f63cad4628d1
Silviu Filip <61214836+rampagex94@users.noreply.github.com>
2022-04-08 14:28:19 +0200

    a

--- src/calculator/Main3.java
+++ src/calculator/Main3.java
@@ -1,1 +1,40 @@
-package calculator;public class Main3 {    public static void main(String[] args) {       String text = "FastTrackIT";       int number = 9;        Calculator calc = new Calculator ();        System.out.println(calc.sum(2,3.5));        System.out.println(calc.sub(7,2));        System.out.println(calc.div(20,5));        System.out.println(calc.mul(5,5));        LogicalOperations op = new LogicalOperations ();        int biggest = op.checkBiggerNumber(5,3);        System.out.println("The bigger number is " +biggest);        System.out.println(op.checkText("FastTrackIT"));        System.out.println(op.verifyTextAndNumber(text, number));        System.out.println(op.Amount(9));        System.out.println(op.greaterThen(4));    }}
+package calculator;
+
+public class Main3 {
+
+    public static void main(String[] args) {
+
+       String text = "FastTrackIT";
+       int number = 9;
+
+
+        Calculator calc = new Calculator ();
+        System.out.println(calc.sum(2,3.5));
+        System.out.println(calc.sub(7,2));
+        System.out.println(calc.div(20,5));
+        System.out.println(calc.mul(5,5));
+
+        LogicalOperations op = new LogicalOperations ();
+        int biggest = op.checkBiggerNumber(5,3);
+        System.out.println("The bigger number is " +biggest);
+        System.out.println(op.checkText("FastTrackIT"));
+        System.out.println(op.verifyTextAndNumber(text, number));
+        System.out.println(op.Amount(9));
+        System.out.println(op.greaterThen(4));
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
